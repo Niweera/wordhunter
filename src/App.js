74f13092231194981e9f783c04afa0f5f385e924
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-07-15 16:45:46 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>It is prophesied that WordHunter will live here...</p>
+        <p>It is prophesied that WordHunter will live here soon...</p>
       </header>
     </div>
   );
